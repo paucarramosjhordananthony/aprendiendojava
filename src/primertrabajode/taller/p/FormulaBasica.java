d26a9166d11faab881febe40806ed7353e11e01f
OK <OK@DESKTOP-S9S3L92.bbrouter>
2025-08-19 16:04:40 -0500

    tarea FormulaBasica

---
+++ src/primertrabajode/taller/p/FormulaBasica.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FormulaBasica {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa a: ");
+        int a = sc.nextInt();
+        System.out.print("Ingresa b: ");
+        int b = sc.nextInt();
+        System.out.print("Ingresa c: ");
+        int c = sc.nextInt();
+
+        int z = (a + b) * c;
+
+        System.out.println("El resultado es: " + z);
+    }
+}
+
